3dace74b0b64a41a7903653f5f589e982981412a
Max Ward <max.ward@me.com>
2015-12-02 22:32:46 +0000

    Initial Commit

---
+++ RedBlue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedBlue
+//
+//  Created by Max Ward on 02/12/2015.
+//  Copyright © 2015 maxi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
